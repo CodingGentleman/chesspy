aa2bc02aecd396c9c139e8388c931994d31af36b
Kevin Schmid <schmid.kevin.manuel@gmail.com>
2018-10-24 07:08:53 +0200

    Update game.py

--- game.py
+++ game.py
@@ -11,9 +11,9 @@ class Chessman:
 
 	def __str__(self):
 		return self.__class__.__name__.lower() if self.colour == BLACK else self.__class__.__name__
-	
+
 	def isopponent(self, other):
-		return other.colour != self.colour
+		return isinstance(other, Chessman) and other.colour != self.colour
 
 class K(Chessman):
 	def ismoveallowed(self, fromX, fromY, san):
@@ -36,36 +36,50 @@ class N(Chessman):
 		return True
 
 class P(Chessman):
-	def ismoveallowed(self, fromX, fromY, san):
+	def ismoveallowed(self, fromY, fromX, san):
 		self.enpassante = False
-		if abs(fromY-san.getY()) == 1 and board[fromX][san.getY()].isopponent(self):
-			self.enpassante = True
-		return True
+		if self.colour == WHITE and san.getY()-fromY not in range(1,3 if fromY == 2 else 2):
+			return False
+		if self.colour == BLACK and fromY-san.getY() not in range(1,3 if fromY == 7 else 2):
+			return False
+		passante = board.get(fromX, san.getY())
+		if abs(fromX-san.getX()) == 1 and abs(fromY-san.getY()) == 1:
+			if isinstance(passante, san.getClass()) and self.isopponent(passante):
+				self.enpassante = True
+			if (isinstance(san.target(), Chessman) and self.isopponent(san.target())) or self.enpassante:
+				return True
+		if abs(fromX-san.getX()) == 0 and san.target() == ' ':
+			return True
+		return False
 
 class Board:
 	fileToIdx = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
 	def __init__(self):
 		self.board = [[' ' for x in range(8)] for y in range(8)]
 		for san in list(map(lambda y: San(y), 'Ra1 Nb1 Bc1 Qd1 Ke1 Bf1 Ng1 Rh1 a2 b2 c2 d2 e2 f2 g2 h2 Ra8 Nb8 Bc8 Qd8 Ke8 Bf8 Ng8 Rh8 a7 b7 c7 d7 e7 f7 g7 h7'.split())):
-			chessman = san.getClass()(BLACK if san.getX()>3 else WHITE)
+			chessman = san.getClass()(BLACK if san.getY()>3 else WHITE)
 			self.put(san, chessman)
 
 	def move(self, colour, san):
 		chessman = self.pop(colour, san)
 		self.put(san, chessman)
-	
+
 	def put(self, san, chessman):
-		self.board[san.getX()][san.getY()] = chessman
-	
+		self.board[san.getY()][san.getX()] = chessman
+
 	def pop(self, colour, san):
 		for idxR, rank in enumerate(self.board):
 			for idxF, field in enumerate(rank):
 				if isinstance(field, san.getClass()) and field.colour == colour and field.ismoveallowed(idxR, idxF, san):
 					self.board[idxR][idxF] = ' '
-					if chessman.enpassante:
-						board[idxR][san.getY()] = ' '
+					if field.enpassante:
+						self.board[idxR][san.getY()] = ' '
 					return field
 		raise ValueError('Invalid move')
+
+	def get(self, x, y):
+		return self.board[y][x]
+
 
 	def __str__(self):
 		res = '  a b c d e f g h'
@@ -82,20 +96,23 @@ class Board:
 # standard algebraic notation
 class San:
 	fileToIdx = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
-	
+
 	def __init__(self, notation):
 		if not re.match(r'[BRQNK][a-h][1-8]|[BRQNK][a-h]x[a-h][1-8]|[BRQNK][a-h][1-8]x[a-h][1-8]|[BRQNK][a-h][1-8][a-h][1-8]|[BRQNK][a-h][a-h][1-8]|[BRQNK]x[a-h][1-8]|[a-h]x[a-h][1-8]=(B+R+Q+N)|[a-h]x[a-h][1-8]|[a-h][1-8]x[a-h][1-8]=(B+R+Q+N)|[a-h][1-8]x[a-h][1-8]|[a-h][1-8][a-h][1-8]=(B+R+Q+N)|[a-h][1-8][a-h][1-8]|[a-h][1-8]=(B+R+Q+N)|[a-h][1-8]|[BRQNK][1-8]x[a-h][1-8]|[BRQNK][1-8][a-h][1-8]', notation):
 			raise ValueError('{} not a valid san expression'.format(notation))
 		self.notation = notation
-	
+
 	def getClass(self):
 		return globals()[self.notation[0] if self.notation[0] in 'KQRBN' else 'P']
-	
-	def getX(self):
-		return int(self.notation[-1])-1
-	
+
 	def getY(self):
+		return int(self.notation[-1])-1
+
+	def getX(self):
 		return San.fileToIdx[self.notation[-2]]
+	
+	def target(self):
+		return board.get(self.getX(), self.getY())
 
 def alternatecolour():
 	while True:
@@ -107,7 +124,7 @@ colour = alternatecolour()
 while True:
 	print(board)
 	current = next(colour)
-	
+
 	while True:
 		try: 
 			inp = input("\n{}: ".format(current))
