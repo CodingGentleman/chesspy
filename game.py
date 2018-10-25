039f9f78e1e5e34d4c6eaa645104dc26ff5ddf0b
Kevin Schmid <kevin@codinggentleman.com>
2018-10-25 15:59:11 +0200

    refactoring of classes, preparation for history display

--- game.py
+++ game.py
@@ -16,7 +16,6 @@ CLI_RESET = '\033[0m'
 class Chessman(object):
     def __init__(self, colour):
         self.colour = colour
-        self.en_passante = False
 
     def __str__(self):
         return self.__class__.__name__.lower() if self.colour == BLACK else self.__class__.__name__
@@ -27,151 +26,142 @@ class Chessman(object):
     def is_opponent(self, other):
         return isinstance(other, Chessman) and other.colour != self.colour
 
-    def __nearing(self, _from, _to):
-        if _from == _to:
-            return None
-        step = 1 if _to > _from else -1
-        it = _from
-        while it != (_to-step):
-            it += step
-            yield it
-
-    def is_move_allowed(self, fromy, fromx, san):
-        self.dy = abs(san.y-fromy)
-        self.dx = abs(san.x-fromx)
-        self.ity = self.__nearing(fromy, san.y)
-        self.itx = self.__nearing(fromx, san.x)
-        if self.is_placeable(san.target):
-            return self._is_move_allowed(fromy, fromx, san)
-        return False
+    def is_move_allowed(self, mv):
+        return self.is_placeable(mv.target.get) and self._is_move_allowed(mv)
 
 class K(Chessman):
-    def _is_move_allowed(self, fromy, fromx, san):
-        return self.dy < 2 and self.dx < 2
+    def _is_move_allowed(self, mv):
+        return mv.dy < 2 and mv.dx < 2
 
 class R(Chessman):
-    def _is_move_allowed(self, fromy, fromx, san):
-        if (self.dy == 0) != (self.dx == 0):
-            for x in self.itx:
-                if board.get(x, fromy) != FIELD:
-                    return False
-            for y in self.ity:
-                if board.get(fromx, y) != FIELD:
-                    return False
-            return True
-        return False
+    def _is_move_allowed(self, mv):
+        if (mv.dy != 0) == (mv.dx != 0):
+            return False
+        for x in mv.itx:
+            if board.get(x, mv.source.y) != FIELD:
+                return False
+        for y in mv.ity:
+            if board.get(mv.source.x, y) != FIELD:
+                return False
+        return True
 
 class B(Chessman):
-    def _is_move_allowed(self, fromy, fromx, san):
-        if self.dy == self.dx:
-            for x in self.itx:
-                y = next(self.ity)
-                if board.get(x, y) != FIELD:
-                    return False
-            return True
-        return False
+    def _is_move_allowed(self, mv):
+        if mv.dy != mv.dx:
+            return False
+        for x in mv.itx:
+            y = next(mv.ity)
+            if board.get(x, y) != FIELD:
+                return False
+        return True
 
 class Q(R, B):
-    def _is_move_allowed(self, fromy, fromx, san):
-        return R._is_move_allowed(self, fromy, fromx, san) or B._is_move_allowed(self, fromy, fromx, san)
+    def _is_move_allowed(self, mv):
+        return R._is_move_allowed(self, mv) \
+            or B._is_move_allowed(self, mv)
 
 class N(Chessman):
-    def _is_move_allowed(self, fromy, fromx, san):
-        return (self.dy == 2 and self.dx == 1) or (self.dy == 1 and self.dx == 2)
+    def _is_move_allowed(self, mv):
+        return (mv.dy == 2 and mv.dx == 1) or (mv.dy == 1 and mv.dx == 2)
 
 class P(Chessman):
-    def is_move_allowed(self, fromy, fromx, san):
-        self.dy = abs(san.y-fromy)
-        self.dx = abs(san.x-fromx)
-        self.en_passante = False
+    def _is_move_allowed(self, mv):
+        is_on_start_rank = mv.source.y == (1 if self.colour == WHITE else 6)
 
         # check direction
-        if self.colour == WHITE and san.y-fromy not in range(1, 3 if fromy == 1 else 2):
+        if self.colour == WHITE and mv.target.y - mv.source.y < 0:
             return False
-        if self.colour == BLACK and fromy-san.y not in range(1, 3 if fromy == 6 else 2):
+        if self.colour == BLACK and mv.source.y - mv.target.y < 0:
             return False
 
         # check capture
-        if self.dx == 1 and self.dy == 1:
-            passante = board.get(san.x, fromy)
-            if isinstance(passante, san.piece_type) and self.is_opponent(passante):
-                self.en_passante = True
-            if self.is_opponent(san.target) or self.en_passante:
+        if mv.dx == 1 and mv.dy == 1:
+            passante = board.get(mv.target.x, mv.source.y)
+            if isinstance(passante, mv.piece_type) \
+            and self.is_opponent(passante) \
+            and board.history[-1].special == Move.pawn_double:
+                mv.special = Move.en_passante
                 return True
+            return self.is_opponent(mv.target.get)
 
         # check straight move
-        if self.dx == 0 and san.target == FIELD:
+        if mv.dx == 0 and mv.target.get == FIELD:
             # check if free double jump
-            if self.dy == 2 and board.get(san.x, san.y-(1 if self.colour == WHITE else -1)) != FIELD:
-                return False
-            return True
+            if mv.dy == 2:
+                hurdle = board.get(mv.target.x, mv.target.y-(1 if self.colour == WHITE else -1))
+                mv.special = Move.pawn_double
+                return is_on_start_rank and hurdle == FIELD
+            return mv.dy == 1
 
         return False
 
 class Board(object):
-    file_to_idx = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+    start_state = 'Ra1 Nb1 Bc1 Qd1 Ke1 Bf1 Ng1 Rh1 a2 b2 c2 d2 e2 f2 g2 h2 ' \
+                 +'Ra8 Nb8 Bc8 Qd8 Ke8 Bf8 Ng8 Rh8 a7 b7 c7 d7 e7 f7 g7 h7'
     def __init__(self):
         self.winner = None
+        self.__history = []
         self.board = [[FIELD for x in range(8)] for y in range(8)]
-        for san in list(map(lambda y: San(y), 'Ra1 Nb1 Bc1 Qd1 Ke1 Bf1 Ng1 Rh1 a2 b2 c2 d2 e2 f2 g2 h2 Ra8 Nb8 Bc8 Qd8 Ke8 Bf8 Ng8 Rh8 a7 b7 c7 d7 e7 f7 g7 h7'.split())):
-            chessman = san.piece_type(BLACK if san.y > 3 else WHITE)
-            self.board[san.y][san.x] = chessman
+        moves = [Move(p) for p in Board.start_state.split()]
+        for mv in moves:
+            chessman = mv.piece_type(BLACK if mv.target.y > 3 else WHITE)
+            self.board[mv.target.y][mv.target.x] = chessman
 
-    def move(self, colour, san):
-        if san.special is not None:
-            self.special(san.special)
+    def move(self, colour, mv):
+        if mv.special is not None:
+            self.special(mv.special)
         else:
-            chessman = self.pop(colour, san)
-            self.put(san, chessman)
+            chessman = self.pop(colour, mv)
+            self.put(mv, chessman)
+        self.__history.append(mv)
 
-    def put(self, san, chessman):
-        if isinstance(self.get(san.x, san.y), K):
+    def put(self, mv, chessman):
+        if isinstance(self.get(mv.target.x, mv.target.y), K):
             self.winner = chessman.colour
-        self.board[san.y][san.x] = chessman
-        self.try_promote(san)
+        self.board[mv.target.y][mv.target.x] = chessman
+        self.try_promote(mv)
 
-    def pop(self, colour, san):
+    def pop(self, colour, mv):
         for idxR, rank in enumerate(self.board):
             for idxF, field in enumerate(rank):
-                if isinstance(field, san.piece_type) and field.colour == colour and field.is_move_allowed(idxR, idxF, san):
+                mv.source = Position(idxF, idxR)
+                if isinstance(field, mv.piece_type) and field.colour == colour and field.is_move_allowed(mv):
                     self.board[idxR][idxF] = FIELD
-                    if field.en_passante:
-                        self.board[idxR][san.x] = FIELD
+                    if mv.special == Move.en_passante:
+                        self.board[idxR][mv.target.x] = FIELD
                     return field
         raise ValueError('Invalid move')
 
     def get(self, x, y):
         return self.board[y][x]
 
-    def try_promote(self, san):
-        if isinstance(san.target, P) and (san.y == 0 or san.y == 7):
-            self.board[san.y][san.x] = Q(san.target.colour)
+    @property
+    def history(self):
+        return self.__history
+
+    def try_promote(self, mv):
+        if isinstance(mv.target.get, P) and (mv.target.y == 0 or mv.target.y == 7):
+            self.board[mv.target.y][mv.target.x] = Q(mv.target.get.colour)
 
     def win(self, colour):
         self.winner = colour
 
-    def special(self, sanspecial):
-        if sanspecial == San.blackresigned:
+    def special(self, mv_special):
+        if mv_special == Move.black_resigned:
             self.win(WHITE)
-        if sanspecial == San.whiteresigned:
+        if mv_special == Move.white_resigned:
             self.win(BLACK)
 
     def __str__(self):
-        res = '    a   b   c   d   e   f   g   h'
+        res = ['    a   b   c   d   e   f   g   h']
         idx = 8
         for rank in reversed(self.board):
-            res += '\n  '
-            res += cgi('+---+---+---+---+---+---+---+---+\n')
-            res += str(idx)+' '
-            res += cgi('| ')
-            res += cgi(' | ').join(list(map(display_field, rank)))
-            res += cgi(' |')
-            res += ' '+str(idx)
+            res.append('  '+cgi('+---+---+---+---+---+---+---+---+'))
+            res.append(str(idx)+' '+cgi('| ')+cgi(' | ').join(list(map(display_field, rank)))+cgi(' |')+' '+str(idx))
             idx -= 1
-        res += '\n  '
-        res += cgi('+---+---+---+---+---+---+---+---+\n')
-        res += '    a   b   c   d   e   f   g   h'
-        return res
+        res.append('  '+cgi('+---+---+---+---+---+---+---+---+'))
+        res.append('    a   b   c   d   e   f   g   h')
+        return '\n'.join(res)
 
 def display_field(field):
     if field == FIELD:
@@ -184,64 +174,114 @@ def cgi(text, fg=CLI_BOARD, bg=CLI_BG, fw=''):
     return '{}{}{}{}{}'.format(fg, fw, bg, text, CLI_RESET)
 
 # standard algebraic notation
-class San(object):
+class Move(object):
     file_to_idx = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
-    blackresigned = 'br'
-    whiteresigned = 'wr'
+    san_regex = r'^([NBRQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(=[NBRQK])?(\+|#)?$|^O-O(-O)?$|^1-0$|^0-1$'
+    black_resigned = 'br'
+    white_resigned = 'wr'
+    pawn_double = 'pd'
+    en_passante = 'ep'
 
-    def __init__(self, notation):
-        if not re.match(r'^([NBRQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(=[NBRQK])?(\+|#)?$|^O-O(-O)?$|^1-0$|^0-1$', notation):
-            raise ValueError('{} is not a valid SAN expression'.format(notation))
-        self.notation = notation
-        self.special = None
-        self.checkresigned()
+    def __init__(self, san):
+        if not re.match(Move.san_regex, san):
+            raise ValueError('{} is not a valid SAN expression'.format(san))
+        self.san = san
+        self._target = Position(Move.file_to_idx[self.san[-2]], int(self.san[-1])-1)
+        self._source = None
+        self._ity = None
+        self._itx = None
+        self.special = self.__check_special()
+
+    def __str__(self):
+        return self.san
 
     @property
     def piece_type(self):
-        return globals()[self.notation[0] if self.notation[0] in 'KQRBN' else 'P']
+        return globals()[self.san[0] if self.san[0] in 'KQRBN' else 'P']
 
     @property
-    def y(self):
-        return int(self.notation[-1])-1
+    def dy(self):
+        return abs(self._target.y - self._source.y)
 
     @property
-    def x(self):
-        return San.file_to_idx[self.notation[-2]]
+    def dx(self):
+        return abs(self._target.x - self._source.x)
 
     @property
     def target(self):
-        return board.get(self.x, self.y)
+        return self._target
 
-    def checkresigned(self):
-        if self.notation == '1-0':
-            self.special = San.blackresigned
-        elif self.notation == '0-1':
-            self.special = San.whiteresigned
+    @property
+    def source(self):
+        return self._source
 
-def alternatecolour():
+    @property
+    def ity(self):
+        return self._ity
+
+    @property
+    def itx(self):
+        return self._itx
+
+    @source.setter
+    def source(self, value):
+        self._source = value
+        self._ity = nearing(value.y, self.target.y)
+        self._itx = nearing(value.x, self.target.x)
+
+    def __check_special(self):
+        if self.san == '1-0':
+            return Move.black_resigned
+        elif self.san == '0-1':
+            return Move.white_resigned
+        return None
+
+class Position(object):
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def get(self):
+        return board.get(self._x, self._y)
+
+def nearing(_from, _to):
+    if _from == _to:
+        return None
+    step = 1 if _to > _from else -1
+    it = _from
+    while it != (_to-step):
+        it += step
+        yield it
+
+def alternate_colour():
     while True:
         yield WHITE
         yield BLACK
 
 # main flow
-running = True
 board = Board()
-while running:
-    colouralternator = alternatecolour()
-    while board.winner is None:
-        err = '\n'
-        current = next(colouralternator)
-        while True:
-            try:
-                print('\033[H\033[J') # clear screen
-                print('        Welcome to Chess.py!\n Use the standard algebraic notation\n to play.     by codinggentleman.com\n')
-                print(board)
-                inp = input('\n {} {}: '.format(err, current))
-                board.move(current, San(inp))
-            except ValueError as e:
-                err = '{}\n'.format(cgi(e, fg=CLI_RED, bg=''))
-            else:
-                break
+colour_alternator = alternate_colour()
+while board.winner is None:
+    err = '\n'
+    current = next(colour_alternator)
+    while True:
+        try:
+            print('\033[H\033[J\n        Welcome to Chess.py!\n Use the standard algebraic notation\n to play.     by codinggentleman.com\n')
+            print(board)
+            inp = Move(input('\n {} {}: '.format(err, current)))
+            board.move(current, inp)
+        except ValueError as e:
+            err = '{}\n'.format(cgi(e, fg=CLI_RED, bg=''))
+        else:
+            break
 
-    running = input('\n Congratulations to {}!\n New Game?[y/n]'.format(board.winner)) == 'y'
-    board = Board()
+print('\n Congratulations to {}!'.format(board.winner))
